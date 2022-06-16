55c189f29bbc0fd6e35b3dd67e61891058d1650f
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-06-16 15:27:43 +0300

    Fix QaObjectTypeForm.tsx

--- web/src/components/QaObjectType/QaObjectTypeForm/QaObjectTypeForm.tsx
+++ web/src/components/QaObjectType/QaObjectTypeForm/QaObjectTypeForm.tsx
@@ -1,5 +1,7 @@
-import {Form, Label, Submit, SubmitHandler, TextField} from "@redwoodjs/forms";
+import {Form, FormError, Label, Submit, SubmitHandler, TextField, useForm} from "@redwoodjs/forms";
 import { useMutation } from '@redwoodjs/web'
+import {toast, Toaster} from "@redwoodjs/web/toast";
+import {navigate, routes} from "@redwoodjs/router";
 
 
 const CREATE_QA_OBJECT_TYPE = gql`
@@ -15,19 +17,39 @@ interface FormValues {
 }
 
 const QaObjectTypeForm = () => {
-  const [createQaObjectType, {loading,error}] = useMutation(CREATE_QA_OBJECT_TYPE);
+
+  const formMethods = useForm();
+
+  const [createQaObjectType, {loading,error}] = useMutation(
+    CREATE_QA_OBJECT_TYPE,
+    {
+      onCompleted: () => {
+        toast.success("Item created");
+        navigate(routes.objectTypes())
+      },
+      onError: (error) => {
+        toast.error(error.message)
+      },
+    }
+  );
 
   const onSubmit: SubmitHandler<FormValues> = (input) => {
-  createQaObjectType({ variables: { input } } );
-    console.log(input)
+    createQaObjectType({ variables: { input } } );
   }
 
   return (
     <div>
-      <Form onSubmit={onSubmit}>
+      <Toaster />
+      <Form
+        onSubmit={onSubmit}
+        config={{ mode: 'onBlur' }}
+        error={error}
+        formMethods={formMethods}
+      >
+        <FormError error={error} wrapperClassName="form-error" />
         <Label name="name">Name</Label>
         <TextField name="name"  validation={{required:true}}/>
-        <Submit>Submit</Submit>
+        <Submit disabled={loading}>Submit</Submit>
       </Form>
     </div>
   )
