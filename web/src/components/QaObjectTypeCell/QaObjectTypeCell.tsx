31297bf9a7ab23358c4a21f671c8886240a58b92
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-06-16 10:59:23 +0300

    Add QaObjectTypeCell

---
+++ web/src/components/QaObjectTypeCell/QaObjectTypeCell.tsx
@@ -0,0 +1,28 @@
+import type { FindQaObjectTypeQuery, FindQaObjectTypeQueryVariables } from 'types/graphql'
+import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+import QaObjectType from "src/components/QaObjectType/QaObjectType";
+
+export const QUERY = gql`
+  query FindQaObjectTypeQuery($id: Int!) {
+    qaObjectType: qaObjectType(id: $id) {
+      id
+      name
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({
+  error,
+}: CellFailureProps<FindQaObjectTypeQueryVariables>) => (
+  <div style={{ color: 'red' }}>Error: {error.message}</div>
+)
+
+export const Success = ({
+  qaObjectType,
+}: CellSuccessProps<FindQaObjectTypeQuery, FindQaObjectTypeQueryVariables>) => {
+  return <><QaObjectType qaObjectType={qaObjectType} /></>
+}
