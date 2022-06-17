f4a9e45157a26a93461443f5fdf705555980438d
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-06-17 13:36:55 +0300

    Align menu items horizontally

--- web/src/layouts/QaLayout/QaLayout.tsx
+++ web/src/layouts/QaLayout/QaLayout.tsx
@@ -9,8 +9,9 @@ const QaLayout = ({ children }: QaLayoutProps) => {
     <header>
       <h1>QA Tool</h1>
       <ul>
-        <li><Link to={routes.home()}>Home</Link></li>
-        <li><Link to={routes.objectTypes()}>Object Types</Link></li>
+        <li style={{display:'inline', marginRight: "20px"}}><Link to={routes.home()}>Home</Link></li>
+        <li style={{display:'inline', marginRight: "20px"}}><Link to={routes.objectTypes()}>Object Types</Link></li>
+        <li style={{display:'inline', marginRight: "20px"}}><Link to={routes.qaObjects()}>QA Objects</Link></li>
       </ul>
     </header>
     <main>{children}</main>
