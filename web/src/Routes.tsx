b6f77bd4f377d8725dc4fcdc709eb837f52ebf27
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-06-17 13:38:52 +0300

    Remove QaObjectsLayout

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -9,20 +9,19 @@
 
 import { Router, Route, Set } from '@redwoodjs/router'
 import QaLayout from "src/layouts/QaLayout/QaLayout";
-import QaObjectsLayout from "src/layouts/QaObjectsLayout/QaObjectsLayout";
 
 const Routes = () => {
   return (
     <Router>
-      <Set wrap={QaObjectsLayout}>
+      <Set wrap={QaLayout}>
+        <Route path="/object-types" page={ObjectTypesPage} name="objectTypes" />
+        <Route path="/object-type/{id:Int}" page={ObjectTypePage} name="objectType" />
+
         <Route path="/qa-objects/new" page={QaObjectNewQaObjectPage} name="newQaObject" />
         <Route path="/qa-objects/{id:Int}/edit" page={QaObjectEditQaObjectPage} name="editQaObject" />
         <Route path="/qa-objects/{id:Int}" page={QaObjectQaObjectPage} name="qaObject" />
         <Route path="/qa-objects" page={QaObjectQaObjectsPage} name="qaObjects" />
-      </Set>
-      <Set wrap={QaLayout}>
-        <Route path="/object-types" page={ObjectTypesPage} name="objectTypes" />
-        <Route path="/object-type/{id:Int}" page={ObjectTypePage} name="objectType" />
+
         <Route path="/" page={HomePage} name="home" />
       </Set>
       <Route notfound page={NotFoundPage} />
