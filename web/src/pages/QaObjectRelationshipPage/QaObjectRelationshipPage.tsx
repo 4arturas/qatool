4a88b66e0911daf9c0b6456c3efa848ad96a9d0f
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-06-21 08:40:58 +0300

    Add example serverTime function, latter it will be removed

--- web/src/pages/QaObjectRelationshipPage/QaObjectRelationshipPage.tsx
+++ web/src/pages/QaObjectRelationshipPage/QaObjectRelationshipPage.tsx
@@ -1,16 +1,26 @@
 import { MetaTags } from '@redwoodjs/web'
 import QaObjectRelationshipsCell from "src/components/Relationship/QaObjectRelationshipsCell";
+import {useEffect} from "react";
 
 type QaObjectRelationshipPageProps = {
   id: string
 }
 
 const QaObjectRelationshipPage = ({ id }: QaObjectRelationshipPageProps) => {
+
+  async function test()
+  {
+    const serverTime = await fetch('/.redwood/functions/serverTime');
+    console.log((await serverTime.json()).data);
+    // console.log(await serverTime.text());
+  }
+
+
   return (
     <>
       <MetaTags title="QaObjectRelationship" description="QaObjectRelationship page" />
 
-
+      <button onClick={test}>Button</button>
 
       <br/>
       <div style={{margin:'auto', maxWidth:'500px'}}>
