e72b22faec17e2d001e1d03069c5811a792fe51b
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-06-23 15:37:14 +0300

    Change project title

--- web/src/layouts/QaLayout/QaLayout.tsx
+++ web/src/layouts/QaLayout/QaLayout.tsx
@@ -8,7 +8,7 @@ const QaLayout = ({ children }: QaLayoutProps) => {
   return <>
     <div className="rw-scaffold">
       <header className="rw-header">
-        <h1 className="rw-heading rw-heading-primary">QA Tool</h1>
+        <h1 className="rw-heading rw-heading-primary">BBS QA Tool</h1>
       <ul>
         <li style={{display:'inline', marginRight: "20px"}}>
           <Link to={routes.home()} className="rw-link">Home</Link>
