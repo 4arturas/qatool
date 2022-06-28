a87efb35d41d5ebe1c20b2041c7847597b902314
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-06-28 06:46:50 +0300

    Add generatePaymentId function

--- api/src/functions/global.ts
+++ api/src/functions/global.ts
@@ -6,3 +6,9 @@ export function getRandomIntInclusive(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
+
+export function generatePaymentId(prefix:string):string
+{
+  const random = getRandomIntInclusive(1000,9999);
+  return `${prefix}-${new Date().getTime()}-${random}`;
+}
