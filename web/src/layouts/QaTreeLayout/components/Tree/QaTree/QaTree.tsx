71c413e68b6605006aa0117b4abc4096fd502acb
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-06-29 15:47:41 +0300

    Change visualisation in QaTree

--- web/src/layouts/QaTreeLayout/components/Tree/QaTree/QaTree.tsx
+++ web/src/layouts/QaTreeLayout/components/Tree/QaTree/QaTree.tsx
@@ -1,14 +1,14 @@
-import {typeIdToName, typeIdToColor} from "src/global";
+import {typeIdToName, typeIdToColor, typeIdTagMargin} from "src/global";
 import {Link, routes} from "@redwoodjs/router";
 
 const QaTree = ({typeId}) => {
   return (
     <div style={{marginBottom:'9px'}}>
-      <div style={{display:'inline', marginRight:'10px'}}>
-        <Link to={routes.qaObjectsByTypeId({typeId:typeId})} style={{backgroundColor: `${typeIdToColor(typeId)}`}} className='qaObjectTypeClass'>
-          {typeIdToName(typeId)}
+
+        <Link to={routes.qaObjectsByTypeId({typeId:typeId})}>
+          {typeIdTagMargin(typeId)}
         </Link>
-      </div>
+
       <Link to={routes.qaTreeNew({typeId:typeId})}>New</Link>
     </div>
 
