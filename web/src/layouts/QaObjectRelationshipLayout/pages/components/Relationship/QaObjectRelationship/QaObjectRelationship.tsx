1697480bb060b0f8d8b6a369e8d2c5f948ca9f40
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-07-04 08:34:15 +0300

    Fix relationship, needs work

--- web/src/layouts/QaObjectRelationshipLayout/pages/components/Relationship/QaObjectRelationship/QaObjectRelationship.tsx
+++ web/src/layouts/QaObjectRelationshipLayout/pages/components/Relationship/QaObjectRelationship/QaObjectRelationship.tsx
@@ -1,8 +1,22 @@
-import {CASE, getChildrenTypeIdByParentTypeId, TEST, typeIdToColor, typeIdToName} from "src/global";
+import {
+  CASE,
+  COLLECTION,
+  getChildrenTypeIdByParentTypeId,
+  getRandomIntInclusive,
+  SERVER,
+  TEST,
+  typeIdToColor,
+  typeIdToName
+} from "src/global";
 import {Link, navigate, routes} from "@redwoodjs/router";
-import {useState} from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import {toast, Toaster} from "@redwoodjs/web/toast";
+import ObjectEdit from "src/components/ObjectEdit/ObjectEdit";
+import ObjectClone from "src/components/ObjectClone/ObjectClone";
+import ObjectDelete from "src/components/ObjectDelete/ObjectDelete";
+import ObjectNew from "src/components/ObjectNew/ObjectNew";
+import {Tag} from "antd";
 
 const QaObjectRelationship = ({qaObject, uniqueId}) => {
 
@@ -36,11 +50,12 @@ const QaObjectRelationship = ({qaObject, uniqueId}) => {
   }
 
   const childrenIdArr: Array<number> = getChildrenTypeIdByParentTypeId(qaObject.typeId);
-
+  let ctx = 0;
   return (
     <>
-      <div key={`${qaObject.id}${uniqueId++}`}
+      <Tag key={`${qaObject.id}${uniqueId++}`}
             style={{
+              display: 'block',
               whiteSpace: 'nowrap',
               borderRadius: '15px',
               border: '1px solid black',
@@ -53,9 +68,32 @@ const QaObjectRelationship = ({qaObject, uniqueId}) => {
 
         <strong key={`${qaObject.id}${uniqueId++}`} style={{marginRight: '10px'}}>{qaObject.name}</strong>
 
-        <Link key={`${qaObject.id}${uniqueId++}`} to={routes.editQaObject({id: qaObject.id})} style={{marginRight: '10px', textDecoration: 'none', color: 'black'}}>
-          Edit
-        </Link>
+        <Tag key={`edibBlock${qaObject.id}${qaObject.typeId}`} style={{padding: '1px', paddingBottom: '3px', marginBottom: '5px', borderRadius: '15px'}}>
+          <ObjectEdit qaObject={qaObject} beforeSave={()=>{}} afterSave={()=>{}}/>&nbsp;&nbsp;&nbsp;
+          <ObjectClone parentId={(qaObject.typeId===COLLECTION || qaObject.typeId===SERVER) ? null : qaObject.id} qaObject={qaObject} beforeSave={()=>{}} afterSave={(newObject)=>{} }/>&nbsp;&nbsp;&nbsp;
+          <ObjectDelete key={`delete${qaObject.id}`}
+                        id={qaObject.id}
+                        beforeSave={()=>{}}
+                        afterSave={(id)=> {
+                          document.getElementById(`edibBlock${qaObject.id}${qaObject.typeId}`).style.display = 'none';
+                        }
+                        }/>&nbsp;&nbsp;&nbsp;
+          {
+            childrenIdArr.map((tId: number, idx: number) => {
+
+                if ( qaObject.typeId === CASE || qaObject.typeId === TEST )
+                {
+                  const alreadyAddedThisTypeOfObject = qaObject.children.find((c) => c.typeId === tId);
+                  if (alreadyAddedThisTypeOfObject)
+                    return <span key={`${qaObject.id}${qaObject.typeId}${uniqueId++}${idx}${tId}`}></span>
+                }
+
+              return <span key={`${qaObject.id}${qaObject.typeId}${uniqueId++}${idx}${tId}`}>
+                <ObjectNew typeId={tId} parentId={qaObject.id} beforeSave={()=>{}} afterSave={(newObject)=>{} }/>
+              </span>
+              })
+          }
+        </Tag>
 
         { qaObject.parentId && <span key={`${qaObject.id}${uniqueId++}`} id={qaObject.relationshipId} onClick={detachComponent} style={{cursor:'pointer'}}>Detach</span> }
 
@@ -67,27 +105,9 @@ const QaObjectRelationship = ({qaObject, uniqueId}) => {
           </span>
         }
 
-        {childrenIdArr.map((tId: number, idx: number) => {
 
-            if ( qaObject.typeId === CASE || qaObject.typeId === TEST )
-            {
-              const alreadyAddedThisTypeOfObject = qaObject.children.find((c) => c.typeId === tId);
-              if (alreadyAddedThisTypeOfObject)
-                return <></>
-            }
-
-            return <Link key={`${qaObject.id}${uniqueId++}${idx}`} to={routes.qaObjectRelationshipNew({parentId: qaObject.id, typeId: tId})} style={{marginLeft: '10px', textDecoration:'none', color:'black'}}>
-                <span key={`${qaObject.id}${uniqueId++}${idx}`} style={{
-                  backgroundColor: `${typeIdToColor(tId)}`,
-                  border: '1px solid black',
-                  padding: '3px',
-                  borderRadius: '15px'
-                }}>Add {typeIdToName(tId)}</span>
-            </Link>
-          })
-        }
         {qaObject.children.map( (c, idx) => <QaObjectRelationship key={uniqueId+idx} qaObject={c} uniqueId={(uniqueId++)*(idx++)}/> )}
-      </div>
+      </Tag>
     </>
   )
 }
