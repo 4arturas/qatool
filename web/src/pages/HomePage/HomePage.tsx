cdba4c9ffad3aa4c2b4121a5b465eeaf4e551594
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-07-07 07:34:45 +0300

    Remove unused imports

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,4 @@
 import { MetaTags } from '@redwoodjs/web'
-import QaTreesPage from "src/pages/QaTreesPage/QaTreesPage";
-import {DEFAULT_PAGE_SIZE} from "src/global";
-import SearchQaObjects from "src/components/SearchQaObjects/SearchQaObjects";
 
 const HomePage = () => {
   return (
