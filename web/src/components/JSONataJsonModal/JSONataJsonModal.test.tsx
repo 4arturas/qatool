5f260e713416092ef0a58572329d7038cc7d0c26
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-07-07 15:01:45 +0300

    Get rid of ide show errors on JSONataJsonModal.test.tsx

--- web/src/components/JSONataJsonModal/JSONataJsonModal.test.tsx
+++ web/src/components/JSONataJsonModal/JSONataJsonModal.test.tsx
@@ -1,6 +1,6 @@
 import { render } from '@redwoodjs/testing/web'
+import JsoNataJsonModal from "src/components/JSONataJsonModal/JSONataJsonModal";
 
-import JsoNataJsonModal from './JsoNataJsonModal'
 
 //   Improve this test with help from the Redwood Testing Doc:
 //    https://redwoodjs.com/docs/testing#testing-components
@@ -8,7 +8,7 @@ import JsoNataJsonModal from './JsoNataJsonModal'
 describe('JsoNataJsonModal', () => {
   it('renders successfully', () => {
     expect(() => {
-      render(<JsoNataJsonModal />)
+      render(<JsoNataJsonModal title={null} JSONata={null} json={null} />)
     }).not.toThrow()
   })
 })
