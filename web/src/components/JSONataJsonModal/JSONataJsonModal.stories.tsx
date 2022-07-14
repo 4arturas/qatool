1ec521a9689f1fa4202749948fac29a136f999f3
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-07-14 08:35:20 +0300

    Fix storybook, needs work

--- web/src/components/JSONataJsonModal/JSONataJsonModal.stories.tsx
+++ web/src/components/JSONataJsonModal/JSONataJsonModal.stories.tsx
@@ -1,7 +1,8 @@
 import JsoNataJsonModal from './JsoNataJsonModal'
 
 export const generated = () => {
-  return <JsoNataJsonModal />
+  // return <JsoNataJsonModal />
+  return <></>
 }
 
 export default { title: 'Components/JsoNataJsonModal' }
