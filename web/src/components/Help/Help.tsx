aa6339df84819a44f10580a6309747e8dd9373b6
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-07-29 13:03:26 +0300

    Add a little changes into Help

--- web/src/components/Help/Help.tsx
+++ web/src/components/Help/Help.tsx
@@ -145,10 +145,7 @@ const Help = ( { anchor, size= 15 } ) => {
           <h1 id='anchorFunctions0'>Functions on QA Objects</h1>
 
           <h2 id='anchorFunctions1'>
-            Edit&nbsp;
-            <FontAwesomeIcon
-              icon={faPen}
-            />
+            Edit
           </h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
