ce6852838eb38b30ca8ce31e89c651249cb8257f
Art <4arturas@gmail.com>
2022-07-31 20:25:17 +0300

    Make simplifications for BlocklyTree

--- web/src/components/BlocklyTree/BlocklyTree.tsx
+++ web/src/components/BlocklyTree/BlocklyTree.tsx
@@ -57,11 +57,6 @@ const BlocklyTree = ( { id }) => {
 
   const [loading, setLoading] = useState( false );
 
-  const [tree, setTree] = useState(null);
-  const [hierarchy, setHierarchy] = useState([]);
-  const [objects, setObjects] = useState([]);
-  const [qaObject, setQaObject] = useState( null );
-
   const append_ChildBlock = ( parentBlock, inputName:string, appendBlock ) =>
   {
     if ( !parentBlock )
@@ -134,124 +129,31 @@ const BlocklyTree = ( { id }) => {
       fetchTree2( qaObject, childrenQaObject, block, tree );
     } );
   }
-  const fetchTree = ( tmpQaObject, tree ) => {
 
-    if ( tmpQaObject.typeId === EXPERIMENT )
-    {
-      const experiment = tree.objects.find( o => o.id === tmpQaObject.id );
-      const experimentBlock = restore_Object( null, experiment );
-      blockBlock.push( experimentBlock );
-      ///////////////////////////////////////////////////
-      /// SERVER
-      const serverChildrenId = experiment.parent.find( h => h.parentId === experiment.id && h.childrenObjectTypeId === SERVER );
-      if ( serverChildrenId )
-      {
-        const server = tree.objects.find( s => s.id === serverChildrenId.childrenId );
-        const serverBlock = restore_Object( experiment, server );
-        append_ChildBlock( experimentBlock, 'SERVER', serverBlock );
-      }
-
-      ///////////////////////////////////////////////////
-      /// COLLECTION
-      const collectionChildrenIds = experiment.parent.filter( p => p.childrenId && p.childrenObjectTypeId === COLLECTION );
-      if ( collectionChildrenIds.length > 0 )
-      {
-        collectionChildrenIds.map( c => {
-          const collection = tree.objects.find( o => o.id === c.childrenId );
-          let collectionBlock = restore_Object( experiment, collection );
-          append_ChildBlock( experimentBlock, 'COLLECTIONS', collectionBlock );
-          ///////////////////////////////////////////////////
-          /// SUITE
-          const suiteChildrenIds = collection.parent.filter(h => h.parentId === collection.id && h.childrenObjectTypeId === SUITE );
-          if ( suiteChildrenIds.length > 0 )
-          {
-            suiteChildrenIds.map( s => {
-              const suite = tree.objects.find( o => o.id === s.childrenId );
-              let suiteBlock = restore_Object( collection, suite );
-              append_ChildBlock( collectionBlock, 'SUITES', suiteBlock );
-              ///////////////////////////////////////////////////
-              /// CASE
-              const caseChildrenIds = suite.parent.filter( h => h.parentId === suite.id && h.childrenObjectTypeId === CASE );
-              if ( caseChildrenIds.length > 0 )
-              {
-                caseChildrenIds.map( c => {
-                  const cAse = tree.objects.find( o => o.id === c.childrenId );
-                  let caseBlock = restore_Object( suite, cAse );
-                  append_ChildBlock( suiteBlock, 'CASES', caseBlock );
-                  ///////////////////////////////////////////////////
-                  /// BODY
-                  const bodyChildren = cAse.parent.find( c => c.childrenObjectTypeId === BODY );
-                  const body = tree.objects.find( b => b.id === bodyChildren.childrenId );
-                  const bodyBlock = restore_Object( cAse, body );
-                  append_ChildBlock( caseBlock, 'BODY', bodyBlock );
-                  ///////////////////////////////////////////////////
-                  /// TESTS
-                  const testIds = cAse.parent.filter( c => c.childrenObjectTypeId === TEST );
-                  if ( testIds.length > 0 )
-                  {
-                    testIds.map( tId => {
-                      const test = tree.objects.find( t => t.id === tId.childrenId );
-                      const replaceId = test.parent.find( r => r.childrenObjectTypeId === REPLACE ).childrenId;
-                      const removeId = test.parent.find( r => r.childrenObjectTypeId === REMOVE ).childrenId;
-                      const resultId = test.parent.find( r => r.childrenObjectTypeId === RESULT ).childrenId;
-                      const responseId = test.parent.find( r => r.childrenObjectTypeId === RESPONSE ).childrenId;
-                      const replace = tree.objects.find( r => r.id === replaceId );
-                      const remove = tree.objects.find( r => r.id === removeId );
-                      const result = tree.objects.find( r => r.id === resultId );
-                      const response = tree.objects.find( r => r.id === responseId );
-
-                      const testBlock = restore_Object( cAse, test );
-                      append_ChildBlock( caseBlock, 'TESTS', testBlock );
-                      const replaceBlock = restore_Object( test, replace );
-                      const removeBlock = restore_Object( test, remove );
-                      const resultBlock = restore_Object( test, result );
-                      const responseBlock = restore_Object( test, response );
-                      append_ChildBlock( testBlock, 'REPLACE', replaceBlock);
-                      append_ChildBlock( testBlock, 'REMOVE', removeBlock);
-                      append_ChildBlock( testBlock, 'RESULT', resultBlock);
-                      append_ChildBlock( testBlock, 'RESPONSE', responseBlock);
-                    }); // end map tests
-                  }
-                });
-
-              } // end if case
-            }); // end map suites;
-          } // end if suites
-
-        } ); // end map collections
-      }   // end if collections
-    }
-
-
-  }
-
-
-  let workspace;
   useEffect( () => {
-    Blockly.common.defineBlocksWithJsonArray(comp);
-    workspace = ( Blockly.inject('ide', { toolbox: toolbox,   move: {
-        scrollbars: {
-          horizontal: true,
-          vertical: true
-        },
-        drag: true,
-        wheel: true} } ) );
 
     setLoading( true );
     client
       .query( { query: QUERY, variables: { id: id } } )
       .then( ret => {
+        Blockly.common.defineBlocksWithJsonArray(comp);
+
+        const workspace = ( Blockly.inject('ide', { toolbox: toolbox,   move: {
+            scrollbars: {
+              horizontal: true,
+              vertical: true
+            },
+            drag: true,
+            wheel: true} } ) );
+
         const tmpTree = ret.data.tree;
-        setTree( tmpTree );
         const parentId = tmpTree.parentId;
-        setHierarchy( tmpTree.hierarchy );
-        setObjects( tmpTree.objects );
         const tmpQaObject = tmpTree.objects.find( o => o.id === parentId );
-        setQaObject( tmpQaObject );
+
         setLoading(false);
 
         fetchTree2( null, tmpQaObject, null, tmpTree );
-        // fetchTree(tmpQaObject, tmpTree);
+
         Blockly.serialization.workspaces.load(firstBlock, workspace);
       });
   }, [] );
