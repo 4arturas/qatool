6411b33a6bae76532fff3761474ca5fbb43d1446
Art <4arturas@gmail.com>
2022-08-14 06:59:15 +0300

    Fix Merge story, needs work

--- web/src/components/Merge/Merge.stories.tsx
+++ web/src/components/Merge/Merge.stories.tsx
@@ -1,7 +1,8 @@
 import Merge from './Merge'
+import {standard} from "src/components/Merge/Merge.mock";
 
 export const generated = () => {
-  return <Merge />
+  return <Merge  {...standard()} />
 }
 
 export default { title: 'Components/Merge' }
